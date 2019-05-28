61997d20a09f8abe91caffdab16c5f0726b1c1c3
filiplazovic <lazoviccorp@gmail.com>
2019-05-28 22:32:13 +0200

    Updated example

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { render} from 'react-dom';
+import { render } from 'react-dom';
 import Editor from '../../src/index';
+import { keysMap, lifeCycleMap } from "granit-utils";
 const App = () => (
-    <Editor defaultValue="aaaa"/>
+    <Editor
+        defaultValue="aaaa"
+        keysMap={keysMap}
+        lifeCycleMap={lifeCycleMap}
+    />
 );
 render(<App />, document.getElementById("root"));
